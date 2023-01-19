4c93ea4a197bead0ed778d9832a3e47af0265605
KimHao12 <hao.nk@terra-drone.co.jp>
2023-01-19 11:23:01 +0700

    feat: added custom MaterialPageRoute

--- lib/utils/other/navi.dart
+++ lib/utils/other/navi.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
+class CustomRoute<T> extends MaterialPageRoute<T> {
+  CustomRoute({ required WidgetBuilder builder, required RouteSettings settings })
+      : super(builder: builder, settings: settings);
+
+  @override
+  Widget buildTransitions(BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    // if (settings.isInitialRoute)
+    //   return child;
+
+    // Fades between routes. (If you don't want any animation,
+    // just return child.)
+    // return new FadeTransition(opacity: animation, child: child);
+    return child;
+  }
+}
+
 Future newScreen(
     {required Function newScreen, required BuildContext context}) async {
   await Navigator.push(
     context,
-    MaterialPageRoute(builder: (context) => newScreen()),
+    // MaterialPageRoute(builder: (context) => newScreen()),
+    CustomRoute(builder:  (context) => newScreen(), settings: RouteSettings())
   );
 }
